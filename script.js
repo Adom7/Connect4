824cfab5955b55d565c907dcbafbcb8f905cc237
Adom7 <72967617+Adom7@users.noreply.github.com>
2022-07-28 18:41:29 +0200

    logique Victoire empêche modif joueur

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var board;
-var PlayerR = ['R', 'red']
-var PlayerY = ['Y', 'yellow']
+var PlayerR = 'red'
+var PlayerY = 'yellow'
 var currentPlayer = PlayerR;
 var GameOver = false
 
@@ -62,7 +62,6 @@ function setJeton() {
 
     let coords = this.id.split('-')
     let ligne = parseInt(coords[0]);
-    // la ligne sélection par le joueur n'est pas importante car elle sera modifié par la suite du code
     let colonne = parseInt(coords[1]);
 
     // on recupére les coordonnées savoir où le joueur souhaite jouer, et appliquer notre logique au coup qu'il souhaite jouer
@@ -78,18 +77,26 @@ function setJeton() {
     }
 
     board[LowestPlayable][colonne] = currentPlayer;
-    document.getElementById([LowestPlayable] + '-' + [colonne]).innerText = currentPlayer[0];
-    document.getElementById([LowestPlayable] + '-' + [colonne]).style.backgroundColor = currentPlayer[1]
+    document.getElementById([LowestPlayable] + '-' + [colonne]).innerText = currentPlayer
+    document.getElementById([LowestPlayable] + '-' + [colonne]).style.backgroundColor = currentPlayer
+
+    if (gagner(LowestPlayable, colonne, currentPlayer) == true) {
+        console.log(currentPlayer + 'Won the Game , Congrats');
+    }
 
     if (currentPlayer == PlayerR) {
         currentPlayer = PlayerY
     }
     else
         currentPlayer = PlayerR
+
+
+
 }
 
 
-function gagner(CoordsJeton) {
+function gagner(line, column, currentPlayer) {
+    //NOTE 
     // Une fois un jeton jouer, il faut verifier si le joueur a gagner, Pour voir si un jouer a gagner, il faut d'abord verifier si il y a un jeton de même couleur adjacent a ce jeton
     // Nous avons donc dans le 'pire' des cas 7 jetons adjacent a verifier Top-Left, Left, Bottom-left, Bottom, Bottom-Right, Right et Top-Right 
     // on check donc si une de ces possibilité est verifier.  si le jeton joué est Rouge et le jeton Adjagent 'Right' est également rouge
@@ -100,13 +107,33 @@ function gagner(CoordsJeton) {
     // ça semble clair, maintenant il faut le coder :)
 
 
-    // Equations de chaques possibilité
-    let TopLeft = board[ligne + 1][colonne - 1]
-    let Left = board[ligne][colonne - 1]
-    let BottomLeft = board[ligne - 1][colonne - 1]
-    let Bottom = board[ligne - 1][colonne]
-    let BottomRight = board[ligne - 1][colonne + 1]
-    let Right = board[ligne][colonne + 1]
-    let TopRight = board[ligne + 1][colonne + 1]
+    //ANCHOR Equations de chaques possibilité
+    // TopLeft = board[ligne - 1][colonne - 1]
+    // Left = board[ligne][colonne - 1]
+    // BottomLeft = board[ligne + 1][colonne - 1]
+    // Bottom = board[ligne + 1][colonne]
+    // BottomRight = board[ligne + 1][colonne + 1]
+    // Right = board[ligne][colonne + 1]
+    // TopRight = board[ligne - 1][colonne + 1]
+
+
+    let TopLeft = false
+    let Left = false
+    let BottomLeft = false
+    let Bottom = false
+    let BottomRight = false
+    let Right = false
+    let TopRight = false
+
+    console.log(line, column, currentPlayer);
+    console.log(board[line - 1][column - 1]);
+    console.log(board);
+
+
+
+
+
+
+    return false
 
 }
